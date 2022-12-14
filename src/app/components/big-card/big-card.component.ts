ab76a8a916bf6edeee4014301fe248f7627a4616
Lucas Taborda <lucastaborda1970@gmail.com>
2022-12-14 16:09:18 -0300

    Styles and components dinamycs

--- src/app/components/big-card/big-card.component.ts
+++ src/app/components/big-card/big-card.component.ts
@@ -1,10 +1,23 @@
-import { Component } from '@angular/core';
+import { Component, Input} from '@angular/core';
 
 @Component({
   selector: 'app-big-card',
   templateUrl: './big-card.component.html',
-  styleUrls: ['./big-card.component.css']
+  styleUrls: ['./big-card.component.css','./big-card.responsive.component.css']
 })
 export class BigCardComponent {
+  @Input()
+  photoCover:string= ""
+  @Input()
+  cardTitle:string="Nova versão de Thor em 'God Of War' é revelada"
+  @Input()
+  cardDescription="Agora sim, Thor tem sua aparência revelada em 'God of War' sequência produzida pelo estudio Santa Mônica"
 
+  constructor(){
+
+  }
+
+  ngOnInit(): void {
+
+  }
 }
--- src/app/components/small-card/small-card.component.ts
+++ src/app/components/small-card/small-card.component.ts
@@ -1,10 +1,19 @@
-import { Component } from '@angular/core';
+import { Component, Input } from '@angular/core';
 
 @Component({
   selector: 'app-small-card',
   templateUrl: './small-card.component.html',
-  styleUrls: ['./small-card.component.css']
+  styleUrls: ['./small-card.component.css','./small-card.responsive.component.css']
 })
 export class SmallCardComponent {
+  @Input()
+  photoCover:string ="https://thelastofus.com.br/wp-content/uploads/2022/07/Voz-de-Joel-em-The-Last-of-Us-no-Brasil-Luiz-Carlos-Persy-esta-oferecendo-curso-de-dublagem-1024x577.jpg"
+  @Input()
+  cardTitle:string="Teste 123"
+  constructor (){
 
+  }
+  ngOnInit(): void{
+
+  }
 }
